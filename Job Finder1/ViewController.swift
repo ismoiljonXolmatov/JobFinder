ae8f303525c40a8507ecdfdd4de6c39a7e73fafd
IosDevelopUZ <126049891+IosDevelopUZ@users.noreply.github.com>
2023-03-10 20:10:05 +0500

    Initial Commit

---
+++ Job Finder1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Job Finder1
+//
+//  Created by Apple on 18.08.1444 (AH).
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
